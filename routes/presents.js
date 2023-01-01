2269ff93c97f46abb7a82a416d4ef8df6cc4eb99
Alberto Valenzuela <avalen28@gmail.com>
2023-01-01 21:05:24 +0100

    iterations 2-3 done. styles WIP

--- routes/presents.js
+++ routes/presents.js
@@ -8,6 +8,28 @@ router.get("/", (req, res, next) => {
     .then((presentsFromDB) => res.render("all-presents", { presentsFromDB }))
     .catch((err) => console.log(err));
 });
+
+router.get("/new", (req, res, next) => res.render("new-present"));
+router.post("/new", (req, res, next) => {
+  const { name, image, price, recipient, description } = req.body;
+  //const price = Number(req.body.price) porque aunque lo fuerza, se lo pasamos previamente como un string;
+  Present.create({
+    name,
+    image,
+    price,
+    recipient,
+    description,
+  })
+    .then(() => res.redirect("/presents"))
+    .catch((err) => console.log(err));
+});
+router.get("/delete/:id", (req, res, next) => {
+  const { id } = req.params;
+  Present.findByIdAndDelete(id)
+    .then(() => res.redirect("/presents"))
+    .catch((err) => console.log(err));
+});
+
 router.get("/:presentId", (req, res, next) => {
   const { presentId } = req.params;
   Present.findById(presentId).then((presentFromDB) =>
